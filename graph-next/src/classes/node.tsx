27a32e50dadbf835563213a1990b6276f255e468
groverongo <ugartegrover@gmail.com>
2025-10-11 02:30:08 -0500

    new logic

---
+++ graph-next/src/classes/node.tsx
@@ -0,0 +1,82 @@
+import { FONT_SIZE, NODE_RADIUS } from "@/common/constant";
+import { SelectedObjectType } from "@/common/types";
+import { drawText, strokeThemeBased } from "@/common/utilities";
+import Konva from "konva";
+import { KonvaEventObject } from "konva/lib/Node";
+import { Ref, useEffect, useImperativeHandle, useRef, useState } from "react";
+import { Circle, Group, Text } from "react-konva";
+
+export type NodeGRef = {
+  x: number;
+  y: number;
+  isSelected: boolean;
+  select: () => void;
+  deselect: () => void;
+  appendCharacter: (character: string) => void;
+  deleteCharacter: () => void;
+};
+
+export default function NodeG({
+  ref,
+  x,
+  y,
+  onSelect,
+}: {
+  ref?: Ref<NodeGRef>;
+  x: number;
+  y: number;
+  onSelect?: () => void;
+}) {
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  const [text, setText] = useState<string>("");
+
+  const GroupRef = useRef<Konva.Group>(null);
+
+  useImperativeHandle(ref, () => ({
+    x: GroupRef.current?.x() || x,
+    y: GroupRef.current?.y() || y,
+    isSelected: isSelected,
+    select: () => {
+      setIsSelected(true);
+    },
+    deselect: () => {
+      setIsSelected(false);
+    },
+    appendCharacter: (character: string) => {
+      setText((prev) => prev + character);
+    },
+    deleteCharacter: () => {
+      if (text.length === 0) return;
+      setText((prev) => prev.substring(0, prev.length - 1));
+    },
+  }));
+
+  useEffect(() => {
+    if (isSelected) {
+      onSelect?.();
+    }
+  }, [isSelected]);
+
+  return (
+    <Group
+      ref={GroupRef}
+      onClick={() => {
+        setIsSelected(!isSelected);
+      }}
+      onDragStart={() => {
+        if (isSelected) return;
+        setIsSelected(true);
+      }}
+      draggable
+    >
+      <Circle
+        x={x}
+        y={y}
+        radius={NODE_RADIUS}
+        fill={"white"}
+        stroke={isSelected ? "blue" : "black"}
+      />
+      <Text text={text} x={x} y={y} fontSize={FONT_SIZE} fill="black" />
+    </Group>
+  );
+}
--- graph-next/src/components/dynamic/Canvas.tsx
+++ graph-next/src/components/dynamic/Canvas.tsx
@@ -1,5 +1,5 @@
 import Link from "@/classes/link";
-import NodeC from "@/classes/node";
+import NodeC, { NodeGRef } from "@/classes/node";
 import SelfLink from "@/classes/self_link";
 import StartLink from "@/classes/start_link";
 import { TemporaryLink } from "@/classes/temporary_link";
@@ -24,13 +24,16 @@ import { useAtom, useAtomValue } from "jotai";
 import { useSetAtom } from "jotai";
 import {
   CSSProperties,
+  KeyboardEventHandler,
   MouseEventHandler,
+  RefObject,
   useEffect,
   useRef,
   useState,
 } from "react";
 import { Circle, Layer, Rect, Stage, Text } from "react-konva";
-import Konva from 'konva';
+import Konva from "konva";
+import NodeG from "@/classes/node";
 
 export default function Canvas() {
   const { canvasRef } = useElementRef();
@@ -54,377 +57,88 @@ export default function Canvas() {
 
   useEffect(() => {
     setStyleProps({
-      height: `${700}px`,
-      width: `${(1200 * screen.width) / 2000}px`,
+      height: 700,
+      width: (1200 * screen.width) / 2000,
     });
   }, []);
 
-  useEffect(() => {
-    return () => {
-      if (caretTimer) clearInterval(caretTimer);
-    };
-  }, [caretTimer]);
-
-  function resetCaret() {
-    const handler: Function = () => {
-      const canvas = canvasRef.current;
-      if (canvas === null) return;
-      const context = canvas.getContext("2d");
-      if (context === null) return;
-      caretVisibleRef.current = !caretVisibleRef.current;
-      draw(
-        canvas,
-        context,
-        nodes,
-        links,
-        currentLink,
-        theme,
-        selectedObject,
-        caretVisibleRef.current,
-        inCanvasRef.current,
-        setJsonEditor
-      );
-    };
-
-    clearInterval(caretTimer);
-
-    setCaretTimer(setInterval(handler, 500));
-
-    caretVisibleRef.current = true;
-  }
-
-  function selectObject(x: number, y: number) {
-    for (let i = 0; i < nodes.length; i++) {
-      if (nodes[i].containsPoint(x, y)) {
-        return nodes[i];
-      }
-    }
-    for (let i = 0; i < links.length; i++) {
-      const l = links[i];
-      if (!(l instanceof TemporaryLink) && l.containsPoint(x, y)) {
-        return l;
-      }
-    }
-    return null;
-  }
-
-  function snapNode(node: NodeC) {
-    for (let i = 0; i < nodes.length; i++) {
-      if (nodes[i] === node) continue;
-
-      if (Math.abs(node.x - nodes[i].x) < SNAP_TO_PADDING) {
-        node.x = nodes[i].x;
-      }
-
-      if (Math.abs(node.y - nodes[i].y) < SNAP_TO_PADDING) {
-        node.y = nodes[i].y;
-      }
-    }
-
-    return node;
-  }
-
-  const onMouseDown: MouseEventHandler<HTMLCanvasElement> = (e) => {
-    console.log(CANVAS_ACTIONS.MOUSE_DOWN.start);
-
-    const canvas = canvasRef.current;
-    if (canvas === null) {
-      console.log(CANVAS_CONDITIONS.MISSING_CANVAS_REF);
-      console.log(CANVAS_ACTIONS.MOUSE_DOWN.end);
-      return;
-    }
-    const context = canvas.getContext("2d");
-    if (context === null) {
-      console.log(CANVAS_CONDITIONS.MISSING_CANVAS_CONTEXT);
-      console.log(CANVAS_ACTIONS.MOUSE_DOWN.end);
-      return;
-    }
-
-    const mouse = crossBrowserRelativeMousePos(e);
-    inCanvasRef.current = true;
-
-    // if selectedObject is not null than save the json of the editor into the node
-    const json = editor;
-    if (
-      selectedObject != null &&
-      (selectedObject instanceof NodeC ||
-        selectedObject instanceof Link ||
-        selectedObject instanceof SelfLink)
-    ) {
-      selectedObject.setJsonModel(json);
-    }
-
-    movingObjectRef.current = false;
-    originalClickRef.current = mouse;
-
-    setSelectedObject(selectObject(mouse.x, mouse.y));
-    if (selectedObject != null) {
-      if (shiftRef.current && selectedObject instanceof NodeC) {
-        setCurrentLink(new SelfLink(selectedObject, mouse));
-      } else {
-        movingObjectRef.current = true;
-        deltaMouseX.current = deltaMouseY.current = 0;
-        if (selectedObject instanceof NodeC) {
-          selectedObject.setMouseStart(mouse.x, mouse.y);
-        } else if (selectedObject instanceof SelfLink) {
-          selectedObject.setMouseStart(mouse.x, mouse.y);
-        }
-      }
-      resetCaret();
-    } else if (shiftRef.current) {
-      setCurrentLink(
-        new TemporaryLink(
-          new NodeC(mouse.x, mouse.y),
-          new NodeC(mouse.x, mouse.y)
-        )
-      );
-    }
-
-    draw(
-      canvas,
-      context,
-      nodes,
-      links,
-      currentLink,
-      theme,
-      selectedObject,
-      caretVisibleRef.current,
-      inCanvasRef.current,
-      setJsonEditor
-    );
-
-    if (inCanvasRef.current) {
-      // disable drag-and-drop only if the canvas is already focused
-      console.log(CANVAS_ACTIONS.MOUSE_DOWN.end);
-      return false;
-    } else {
-      // otherwise, let the browser switch the focus away from wherever it was
-      resetCaret();
-      console.log(CANVAS_ACTIONS.MOUSE_DOWN.end);
-      return true;
-    }
-  };
-
-  const onDoubleClick: MouseEventHandler<HTMLCanvasElement> = (e) => {
-    console.log(CANVAS_ACTIONS.DOUBLE_CLICK.start);
-
-    const canvas = canvasRef.current;
-    if (canvas === null) {
-      console.log(CANVAS_CONDITIONS.MISSING_CANVAS_REF);
-      console.log(CANVAS_ACTIONS.DOUBLE_CLICK.end);
-      return;
-    }
-    const context = canvas.getContext("2d");
-    if (context === null) {
-      console.log(CANVAS_CONDITIONS.MISSING_CANVAS_CONTEXT);
-      console.log(CANVAS_ACTIONS.DOUBLE_CLICK.end);
-      return;
-    }
-
-    const mouse = crossBrowserRelativeMousePos(e);
-    setSelectedObject(selectObject(mouse.x, mouse.y));
-
-    if (selectedObject === null) {
-      const newObject = new NodeC(mouse.x, mouse.y);
-      newObject.nodeId = uniqueId();
-      setSelectedObject(newObject);
-      setNodes([...nodes, newObject]);
-      resetCaret();
-      if (canvasRef.current === null) return;
-      draw(
-        canvas,
-        context,
-        nodes,
-        links,
-        currentLink,
-        theme,
-        newObject,
-        caretVisibleRef.current,
-        inCanvasRef.current,
-        setJsonEditor
-      );
-    } else if (selectedObject instanceof NodeC) {
-      setSelectedObject((prev) => {
-        if (!(prev instanceof NodeC)) {
-          return prev;
-        }
-        prev.isAcceptState = !prev.isAcceptState;
-        if (prev.isAcceptState)
-          draw(
-            canvas,
-            context,
-            nodes,
-            links,
-            currentLink,
-            theme,
-            selectedObject,
-            caretVisibleRef.current,
-            inCanvasRef.current,
-            setJsonEditor
-          );
-        return prev;
-      });
-    }
-
-    console.log(CANVAS_ACTIONS.DOUBLE_CLICK.end);
-  };
-
-  const onMouseMove: MouseEventHandler<HTMLCanvasElement> = (e) => {
-    console.log(CANVAS_ACTIONS.MOUSE_MOVE.start);
-
-    const canvas = canvasRef.current;
-    if (canvas === null) {
-      console.log(CANVAS_CONDITIONS.MISSING_CANVAS_REF);
-      console.log(CANVAS_ACTIONS.MOUSE_MOVE.end);
-      return;
-    }
-    const context = canvas.getContext("2d");
-    if (context === null) {
-      console.log(CANVAS_CONDITIONS.MISSING_CANVAS_CONTEXT);
-      console.log(CANVAS_ACTIONS.MOUSE_MOVE.end);
-      return;
-    }
-
-    const mouse = crossBrowserRelativeMousePos(e);
-
-    if (currentLink != null) {
-      let targetNode = selectObject(mouse.x, mouse.y);
-      if (!(targetNode instanceof NodeC)) {
-        targetNode = null;
-      }
-
-      if (selectedObject == null) {
-        if (targetNode != null) {
-          originalClickRef.current &&
-            setCurrentLink(new StartLink(targetNode, originalClickRef.current));
-        } else {
-          originalClickRef.current &&
-            setCurrentLink(
-              new TemporaryLink(
-                new NodeC(
-                  originalClickRef.current.x,
-                  originalClickRef.current.y
-                ),
-                new NodeC(mouse.x, mouse.y)
-              )
-            );
-        }
-      } else {
-        if (targetNode === selectedObject) {
-          setCurrentLink(new SelfLink(selectedObject, mouse));
-        } else if (targetNode !== null) {
-          if (selectedObject instanceof NodeC)
-            setCurrentLink(new Link(selectedObject, targetNode));
-        } else {
-          if (selectedObject instanceof NodeC) {
-            const closestPoint = selectedObject.closestPointOnCircle(
-              mouse.x,
-              mouse.y
-            );
-            setCurrentLink(
-              new TemporaryLink(
-                new NodeC(closestPoint.x, closestPoint.y),
-                new NodeC(mouse.x, mouse.y)
-              )
-            );
-          }
-        }
-      }
-      draw(
-        canvas,
-        context,
-        nodes,
-        links,
-        currentLink,
-        theme,
-        selectedObject,
-        caretVisibleRef.current,
-        inCanvasRef.current,
-        setJsonEditor
-      );
-    }
-
-    if (movingObjectRef.current && selectedObject !== null) {
-      selectedObject.setAnchorPoint(mouse.x, mouse.y);
-      if (selectedObject instanceof NodeC) {
-        setSelectedObject(snapNode(selectedObject));
-      }
-      draw(
-        canvas,
-        context,
-        nodes,
-        links,
-        currentLink,
-        theme,
-        selectedObject,
-        caretVisibleRef.current,
-        inCanvasRef.current,
-        setJsonEditor
-      );
-    }
-
-    console.log(CANVAS_ACTIONS.MOUSE_MOVE.end);
-  };
-
-  const onMouseUp: MouseEventHandler<HTMLCanvasElement> = (e) => {
-    console.log(CANVAS_ACTIONS.MOUSE_UP.start);
-
-    const canvas = canvasRef.current;
-    if (canvas === null) {
-      console.log(CANVAS_CONDITIONS.MISSING_CANVAS_REF);
-      console.log(CANVAS_ACTIONS.MOUSE_UP.end);
-      return;
-    }
-    const context = canvas.getContext("2d");
-    if (context === null) {
-      console.log(CANVAS_CONDITIONS.MISSING_CANVAS_CONTEXT);
-      console.log(CANVAS_ACTIONS.MOUSE_UP.end);
-      return;
-    }
-
-    movingObjectRef.current = false;
-
-    if (currentLink != null) {
-      if (!(currentLink instanceof TemporaryLink)) {
-        setSelectedObject(currentLink);
-        currentLink.linkId = uniqueId();
-        setLinks([...valueLinks, currentLink]);
-        resetCaret();
-      }
-      setCurrentLink(null);
-      draw(
-        canvas,
-        context,
-        nodes,
-        links,
-        currentLink,
-        theme,
-        selectedObject,
-        caretVisibleRef.current,
-        inCanvasRef.current,
-        setJsonEditor
-      );
-    }
-    shiftRef.current = false;
-
-    console.log(CANVAS_ACTIONS.MOUSE_UP.end);
-  };
 
   const shapeRef = useRef<Konva.Rect | null>(null);
 
-  const positions = useState<{x: number, y: number}[]>([]);
+  const positions = useState<{ x: number; y: number }[]>([]);
+  const positionRefs = useRef<(NodeGRef | null)[]>([]);
+  const [currentIndex, setCurrentIndex] = useState<number | null>(null);
+
+  const onKeyDown: KeyboardEventHandler<HTMLDivElement> = (e) => {
+      
+    if (currentIndex === null) return;
+    
+    if (positionRefs.current[currentIndex] === null) return;
+    
+    const ref = positionRefs.current[currentIndex];
+    
+    if (ref === null) return;
+    
+    if (e.key.length === 1 && /^[a-zA-Z0-9]$/.test(e.key)) {
+        ref.appendCharacter(e.key);
+    } else if (e.key === "Backspace") {
+        ref.deleteCharacter();
+    }
+  };
+
+  useEffect(() => {
+    console.log(positions[0].length);
+    positionRefs.current = positions[0].map(
+      (_, index) => positionRefs.current[index] ?? null
+    );
+
+    if (currentIndex !== null) {
+      positionRefs.current[currentIndex]?.deselect();
+      positionRefs.current[positionRefs.current.length -1]?.select();
+    }
+
+    positionRefs.current.map((ref, index) => {
+      if (ref !== null) {
+        console.log(index, ref.x, ref.y);
+      } else {
+        console.log(index, "null");
+      }
+    });
+  }, [positions[0].length]);
 
   return (
-    <>
-      <Stage width={window.innerWidth} height={window.innerHeight} onDblClick={(e) => {
-        positions[1]((prev) => [...prev, {x: e.evt.offsetX, y: e.evt.offsetY}]);
-      }}>
+    <div onKeyDown={onKeyDown} tabIndex={0}>
+      <Stage
+        id="KonvaStage"
+        style={styleProps}
+        width={styleProps.width as number}
+        height={styleProps.height as number}
+        onDblClick={(e) => {
+          positions[1]((prev) => [
+            ...prev,
+            { x: e.evt.offsetX, y: e.evt.offsetY },
+          ]);
+        }}
+      >
         <Layer>
-            {positions[0].map((pos, index) => (
-                <Circle key={index} x={pos.x} y={pos.y} radius={50} fill="green" draggable onDblClick={(e) => {
-                    positions[1]((prev) => prev.filter((_, i) => i !== index));
-                }}/>
-            ))}
+          <NodeG x={100} y={100} />
+
+          {positions[0].map((pos, index) => (
+            <NodeG
+              key={index}
+              ref={(e) => {
+                positionRefs.current[index] = e;
+              }}
+              x={pos.x}
+              y={pos.y}
+              onSelect={() => {
+                if (currentIndex !== null) {
+                  positionRefs.current[currentIndex]?.deselect();
+                }
+                setCurrentIndex(index);
+              }}
+            />
+          ))}
           <Text text="Try to drag shapes" fontSize={15} />
           <Rect
             x={20}
@@ -436,23 +150,13 @@ export default function Canvas() {
             draggable
             ref={shapeRef}
             onDragMove={(e) => {
-            //   console.log(JSON.stringify(e));
-                console.log(shapeRef.current?.x());
+              //   console.log(JSON.stringify(e));
+              console.log(shapeRef.current?.x());
             }}
           />
-          <Circle x={200} y={100} radius={50} fill="green" draggable/>
+          <Circle x={200} y={100} radius={50} fill="green" draggable />
         </Layer>
       </Stage>
-      <canvas
-        className="rounded bg-white shadow-lg"
-        ref={canvasRef}
-        id="canvas"
-        style={styleProps}
-        onMouseDown={onMouseDown}
-        onMouseMove={onMouseMove}
-        onDoubleClick={onDoubleClick}
-        onMouseUp={onMouseUp}
-      />
-    </>
+    </div>
   );
 }
